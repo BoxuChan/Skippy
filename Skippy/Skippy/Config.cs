d30fd518a4b364227cae9352d28fab14f59043a0
BoxuChan <enzolefebvre@gmail.com>
2025-12-20 00:25:49 +0000

    Update 7.4 & API 14

--- Skippy/Skippy/Config.cs
+++ Skippy/Skippy/Config.cs
@@ -1,14 +1,12 @@
-﻿using System.ComponentModel;
+﻿using System;
 using Dalamud.Configuration;
 
 namespace Plugins.a08381.Skippy
 {
+    [Serializable]
     public class Config : IPluginConfiguration
     {
-
-        public int Version { get; set; }
-
-        [DefaultValue(true)]
-        public bool IsEnabled { get; set; }
+        public int Version { get; set; } = 1;
+        public bool IsEnabled { get; set; } = true;
     }
 }
--- Skippy/Skippy/Skippy.cs
+++ Skippy/Skippy/Skippy.cs
@@ -1,14 +1,9 @@
 using System;
 using System.Diagnostics;
-using System.Diagnostics.CodeAnalysis;
-using System.Net;
 using System.Security.Cryptography;
 using Dalamud;
 using Dalamud.Game;
 using Dalamud.Game.Command;
-using Dalamud.Game.Gui;
-using Dalamud.IoC;
-using Dalamud.Logging;
 using Dalamud.Plugin;
 using Dalamud.Plugin.Services;
 
@@ -16,41 +11,56 @@ namespace Plugins.a08381.Skippy
 {
     public class Skippy : IDalamudPlugin
     {
+        public string Name => "Skippy";
 
         private readonly Config _config;
         private readonly RandomNumberGenerator _csp;
-
         private readonly decimal _base = uint.MaxValue;
+        private readonly IDalamudPluginInterface _pluginInterface;
+        private readonly ISigScanner _sigScanner;
+        private readonly ICommandManager _commandManager;
+        private readonly IChatGui _chatGui;
+        private readonly IPluginLog _pluginLog;
 
-        [SuppressMessage("ReSharper", "PossibleNullReferenceException")]
-        [SuppressMessage("ReSharper", "AssignNullToNotNullAttribute")]
-        public Skippy()
+        public CutsceneAddressResolver Address { get; }
+
+        public Skippy(
+            IDalamudPluginInterface pluginInterface,
+            ISigScanner sigScanner,
+            ICommandManager commandManager,
+            IChatGui chatGui,
+            IPluginLog pluginLog)
         {
-            if (Interface.GetPluginConfig() is not Config configuration || configuration.Version == 0)
+            _pluginInterface = pluginInterface;
+            _sigScanner = sigScanner;
+            _commandManager = commandManager;
+            _chatGui = chatGui;
+            _pluginLog = pluginLog;
+
+            _csp = RandomNumberGenerator.Create();
+
+            if (_pluginInterface.GetPluginConfig() is not Config configuration || configuration.Version == 0)
                 configuration = new Config { IsEnabled = true, Version = 1 };
 
             _config = configuration;
 
-            Address = new CutsceneAddressResolver();
-
-            Address.Setup(SigScanner);
+            Address = new CutsceneAddressResolver(_pluginLog); 
+            Address.Setup(_sigScanner);
 
             if (Address.Valid)
             {
-                PluginLog.Information("Cutscene Offset Found.");
+                _pluginLog.Information("Cutscene Offset Found.");
                 if (_config.IsEnabled)
                     SetEnabled(true);
             }
             else
             {
-                PluginLog.Error("Cutscene Offset Not Found.");
-                PluginLog.Warning("Plugin Disabling...");
-                Dispose();
+                _pluginLog.Error("Cutscene Offset Not Found.");
+                _pluginLog.Warning("Plugin Disabling...");
                 return;
             }
-            _csp = RandomNumberGenerator.Create();
 
-            CommandManager.AddHandler("/sc", new CommandInfo(OnCommand)
+            _commandManager.AddHandler("/sc", new CommandInfo(OnCommand)
             {
                 HelpMessage = "/sc: Roll your sanity check dice."
             });
@@ -59,26 +69,14 @@ namespace Plugins.a08381.Skippy
         public void Dispose()
         {
             SetEnabled(false);
+            _commandManager.RemoveHandler("/sc");
             GC.SuppressFinalize(this);
         }
-
-        public string Name => "Skippy";
-        
-        [PluginService] public static IDalamudPluginInterface Interface { get; private set; }
-        
-        [PluginService] public static ISigScanner SigScanner { get; private set; }
-
-        [PluginService] public static ICommandManager CommandManager { get; private set; }
-        
-        [PluginService] public static IChatGui ChatGui { get; private set; }
-
-        [PluginService] public static IPluginLog PluginLog { get; private set; }
-
-        public CutsceneAddressResolver Address { get; }
 
         public void SetEnabled(bool isEnable)
         {
             if (!Address.Valid) return;
+            
             if (isEnable)
             {
                 SafeMemory.Write<short>(Address.Offset1, -28528);
@@ -97,17 +95,25 @@ namespace Plugins.a08381.Skippy
             byte[] rndSeries = new byte[4];
             _csp.GetBytes(rndSeries);
             int rnd = (int)Math.Abs(BitConverter.ToUInt32(rndSeries, 0) / _base * 50 + 1);
-            ChatGui.Print(_config.IsEnabled
+            
+            _chatGui.Print(_config.IsEnabled
                 ? $"sancheck: 1d100={rnd + 50}, Failed"
                 : $"sancheck: 1d100={rnd}, Passed");
+            
             _config.IsEnabled = !_config.IsEnabled;
             SetEnabled(_config.IsEnabled);
-            Interface.SavePluginConfig(_config);
+            _pluginInterface.SavePluginConfig(_config);
         }
     }
 
     public class CutsceneAddressResolver : BaseAddressResolver
     {
+        private readonly IPluginLog _log;
+
+        public CutsceneAddressResolver(IPluginLog log)
+        {
+            _log = log;
+        }
 
         public bool Valid => Offset1 != IntPtr.Zero && Offset2 != IntPtr.Zero;
 
@@ -118,15 +124,14 @@ namespace Plugins.a08381.Skippy
         {
             Offset1 = sig.ScanText("75 ?? 48 8b 0d ?? ?? ?? ?? ba ?? 00 00 00 48 83 c1 10 e8 ?? ?? ?? ?? 83 78 ?? ?? 74");
             Offset2 = sig.ScanText("74 18 8B D7 48 8D 0D");
-            Skippy.PluginLog.Information(
-                "Offset1: [\"ffxiv_dx11.exe\"+{0}]",
-                (Offset1.ToInt64() - Process.GetCurrentProcess().MainModule!.BaseAddress.ToInt64()).ToString("X")
-                );
-            Skippy.PluginLog.Information(
-                "Offset2: [\"ffxiv_dx11.exe\"+{0}]",
-                (Offset2.ToInt64() - Process.GetCurrentProcess().MainModule!.BaseAddress.ToInt64()).ToString("X")
-                );
-        }
 
+            var baseAddr = Process.GetCurrentProcess().MainModule!.BaseAddress.ToInt64();
+
+            if (Offset1 != IntPtr.Zero)
+                _log.Information("Offset1: [\"ffxiv_dx11.exe\"+{0:X}]", Offset1.ToInt64() - baseAddr);
+            
+            if (Offset2 != IntPtr.Zero)
+                _log.Information("Offset2: [\"ffxiv_dx11.exe\"+{0:X}]", Offset2.ToInt64() - baseAddr);
+        }
     }
 }
